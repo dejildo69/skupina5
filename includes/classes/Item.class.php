a7503eabadf8a92d133dc19584c32766ce4739d6
Mitja Kramberger <kramberger.mitja@gmail.com>
2012-04-12 23:26:53 +0200

    Amadej Mlinarič

---
+++ includes/classes/Item.class.php
@@ -0,0 +1,25 @@
+<?php
+
+class Item extends Database {
+	
+	public function __construct() 
+	{
+		if(!parent::$connection) { parent::__construct(); }
+	}
+
+	
+	public function getRandomName($length = false)
+	{
+		$consonants = array('q','w','r','t','z','p','s','d','f','g','h','j','k','l','y','x','c','v','b','n','m'); //0-20
+		$syllables = array('a','e','i','o','u'); //0-4
+		$resulingName = "";
+		if($length == false) { $length = rand(3, 10); }
+		for($i = 0; $i < $length; $i++)
+		{
+			if($i%2 == 0) { $resulingName .= $consonants[rand(0,20)]; } else { $resulingName .= $syllables[rand(0,4)]; }
+		}
+		return ucfirst($resulingName);
+	}
+}
+
+?>
---
+++ includes/classes/Market.class.php
@@ -0,0 +1,25 @@
+<?php
+
+class Market extends Database {
+	
+	private $item;
+	
+	public function __construct() 
+	{
+		if(!parent::$connection) { parent::__construct(); }
+		$this->item = $this->load('Item');
+	}
+	
+	public function answer()
+	{
+		if(isset($_GET['categories'])) { $categories = $this->getCategories(); require_once('pages/blocks/categories.php'); }
+		if(isset($_GET['browse'])) 
+		{ 
+			$items = $this->getItemsOnSale($_GET['browse']); 
+			$category = $this->select('*','item_types',array('ID' => substr($_GET['browse'],7))); 
+			require_once('pages/blocks/shopping_list.php'); 
+		}
+	}
+}
+
+?>
