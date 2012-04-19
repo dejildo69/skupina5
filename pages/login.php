283370dba7ee9e51afed9cb7116adee04a0c07a8
SamoSlaus <samo.slaus@gmail.com>
2012-04-19 17:34:17 +0200

    login

---
+++ pages/login.php
@@ -0,0 +1,12 @@
+<?php if(!isset($_SESSION['account']['ID'])) { ?>
+<form method="POST" action="index.php?login">
+	<table>
+		<tr><td>Username</td><td><input type="text" name="Username"></td></tr>
+		<tr><td>Password</td><td><input type="password" name="Password"></td></tr>
+		<tr><td></td><td><input type="submit" value="Login"></td></tr>
+	</table>
+</form>
+<?php } else { 
+	echo("Welcome back ".$_SESSION['account']['Nickname']."! ");
+	echo('<a href="'.HOME_URL.'index.php?logout">Log out</a>');
+} ?>
